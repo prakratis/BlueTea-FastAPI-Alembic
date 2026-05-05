f6c2e324d0bae737874346ce4d271ac3ff8fef22
= <=>
2026-05-05 19:53:57 +0530

    Finalize Alembic setup with SQLModel and initial migration

--- migrations/env.py
+++ migrations/env.py
@@ -45,7 +45,7 @@ def run_migrations_offline() -> None:
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
-        target_metadata=target_metadata,
+        target_metadata = None,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
